377a05b598c6c1e0d70681728a6e68ec716e1d38
P@ko <tomp12@protonmail.com>
2021-12-18 08:15:18 -0800

    Added server ip and port from config

--- espia_server/__main__.py
+++ espia_server/__main__.py
@@ -3,12 +3,14 @@ import pathlib
 
 import uvicorn
 
+from app.utils import config
+
 
 def main():
     certs_dir = pathlib.Path(pathlib.Path(os.path.realpath(__file__)).parent / 'certs')
     uvicorn.run("espia_server.app.api:app",
-                host="0.0.0.0",
-                port=443,
+                host=config['app'].get('SERVER_IP'),
+                port=int(config['app'].get('SERVER_PORT')),
                 reload=True,
                 ssl_keyfile=certs_dir / f"{os.getenv('ESPIA_ENV')}" / 'key.pem',
                 ssl_certfile=certs_dir / f"{os.getenv('ESPIA_ENV')}" / 'cert.pem')
